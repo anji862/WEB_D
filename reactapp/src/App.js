a827763a7406dfae05d84e5e9c7b20a6e7de6591
Varun Mishra <mishravarunmishra31@gmail.com>
2021-08-09 22:07:38 +0530

    first

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './tour/Menu'
+import Cart from './Cart';
+// import Header from './tour/Header'
+// import Footer from './tour/Footer'
+// import Main from './tour/Main'
+
+const App=()=>{
+    return <>
+        <Menu/>
+        <Cart/>
+    </>
+}
+
+export default App;
---
+++ reactapp/src/Data.js
@@ -0,0 +1,22 @@
+const data=[
+    {
+        id:1,
+        name:'Varun',
+        bday:'21st oct',
+        image:'https://filmfare.wwmindia.com/content/2021/jul/shahrukhkhan11626873132.jpg'
+    },
+    {
+        id:2,
+        name:'Akanksha',
+        bday:'10th Dec',
+        image:'https://images.indianexpress.com/2020/02/Tara-Sutaria-1200-1.jpg'
+    },
+    {
+        id:3,
+        name:'Anjali',
+        bday:'6th May',
+        image:'https://images.indianexpress.com/2020/02/Tara-Sutaria-1200-1.jpg'
+    }
+]
+
+export default data;
